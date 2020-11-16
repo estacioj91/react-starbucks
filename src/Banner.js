1870f5110fa98091405912c21772c134775434f5
jonathan estacio <estacio@jonathans-MBP.hsd1.ca.comcast.net>
2020-11-15 22:53:06 -0800

    three banners down, refactored to seperate component

---
+++ src/Banner.js
@@ -0,0 +1,64 @@
+import React from "react";
+const Banner = ({
+	header,
+	image,
+	text,
+	font,
+	fWeight,
+	direction,
+	button,
+	bgColor,
+}) => {
+	return (
+		<div
+			className="banner"
+			style={{ backgroundColor: bgColor, flexDirection: direction }}
+		>
+			<div className="image-div">
+				<img
+					src={require(`./assets/${image}.png`).default}
+					alt="red banner"
+				/>
+			</div>
+			<div className="text-div">
+				{header}
+				<p style={{ fontSize: font, fontWeight: fWeight }}>{text}</p>
+				{button}
+			</div>
+		</div>
+	);
+};
+export default Banner;
+// <div className="home-banner-red">
+// 				<div>
+{
+	/* <img src={require(`./assets/red-banner.png`).default} alt="red banner" />; */
+}
+// 				</div>
+// <div className="banner-textarea">
+// 	<h3>Wishing you the warmest Rewards</h3>
+// 	<p>
+// 		Starbucks® Rewards members can now pay any way they’d like in
+// 		the app and collect Stars to redeem for free holiday faves.
+// 		Positively jolly.*
+// 	</p>
+// 	<button className="clear-button">Get the app</button>
+// </div>
+// 			</div>
+// 			<div className="home-banner-red">
+// 				<div>
+// 					<img
+// 						src={require(`./assets/holday-cup-banner.png`).default}
+// 						alt="red banner"
+// 					/>
+// 				</div>
+// 				<div className="banner-textarea">
+// 					<h1 style={{ fontWeight: "bold", marginBottom: "1em" }}>
+// 						HOLIDAY YOUR WAY
+// 					</h1>
+// 					<p>So many convenient ways to get your festive favorites.</p>
+// 					<button style={{ width: "15em" }} className="clear-button">
+// 						Explore seasonal drinks
+// 					</button>
+// 				</div>
+// 			</div>
--- src/Homebanner.js
+++ src/Homebanner.js
@@ -1,42 +1,52 @@
 import React from "react";
+import Banner from "./Banner";
 // this component can be split up in the future
 const Homebanner = () => {
 	return (
 		<div className="body-padding">
-			<div className="home-banner-red">
-				<div>
-					<img
-						src={require(`./assets/red-banner.png`).default}
-						alt="red banner"
-					/>
-				</div>
-				<div className="banner-textarea">
-					<h3>Wishing you the warmest Rewards</h3>
-					<p>
-						Starbucks® Rewards members can now pay any way they’d like in
-						the app and collect Stars to redeem for free holiday faves.
-						Positively jolly.*
-					</p>
-					<button className="clear-button">Get the app</button>
-				</div>
-			</div>
-			<div className="home-banner-red">
-				<div>
-					<img
-						src={require(`./assets/holday-cup-banner.png`).default}
-						alt="red banner"
-					/>
-				</div>
-				<div className="banner-textarea">
-					<h1 style={{ fontWeight: "bold", marginBottom: "1em" }}>
-						HOLIDAY YOUR WAY
-					</h1>
-					<p>So many convenient ways to get your festive favorites.</p>
+			<Banner
+				image="red-banner"
+				header={<h3>Wishing you the warmest Rewards</h3>}
+				text="Starbucks® Rewards members can now pay any way they’d like in the app and collect Stars to redeem for free holiday faves. Positively jolly.*"
+				button="Get the app"
+				direction="row-reverse"
+				bgColor="#e1251b"
+				font="1.2em"
+				fWeight="300"
+				button={<button className="clear-button">Get the app</button>}
+			/>
+			<Banner
+				image="holday-cup-banner"
+				header={<h1 style={{ fontWeight: "bold" }}>HOLIDAY YOUR WAY</h1>}
+				text="So many convenient ways to get your festive favorites."
+				button="Explore seasonal drinks"
+				direction="row-reverse"
+				bgColor="#e1251b"
+				font="1.2em"
+				fWeight="300"
+				button={
 					<button style={{ width: "15em" }} className="clear-button">
 						Explore seasonal drinks
 					</button>
-				</div>
-			</div>
+				}
+			/>
+			<Banner
+				image="bread"
+				header={
+					<h1 style={{ fontWeight: "bold" }}>BAKING SPIRITS BRIGHT</h1>
+				}
+				text="The new Cranberry Orange Scone: made with crème fraîche, dried cranberries, orange zest and a drizzle of vanilla icing."
+				button="Explore seasonal drinks"
+				direction="row"
+				bgColor="#1e3932"
+				font="1.2em"
+				fWeight="300"
+				button={
+					<button style={{ width: "15em" }} className="clear-button">
+						Order now
+					</button>
+				}
+			/>
 		</div>
 	);
 };
