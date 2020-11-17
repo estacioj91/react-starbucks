7ab903391b7a1ec1314e37e55e5b2d5918ead7ca
jonathan estacio <estacio@jonathans-MBP.hsd1.ca.comcast.net>
2020-11-16 16:10:32 -0800

    main footer added

---
+++ src/Mainfooter.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Dropdown } from "react-bootstrap";
+const Mainfooter = () => {
+	return (
+		<div className="main-footer body-wrap">
+			<div></div>
+			<div className="social-media">
+				<Dropdown.Divider />
+				<div className="icons">
+					<img src={require(`./assets/s.svg`).default} alt="" />
+					<img src={require(`./assets/fb.svg`).default} alt="" />
+					<img src={require(`./assets/p.svg`).default} alt="" />
+
+					<img src={require(`./assets/tw.svg`).default} alt="" />
+					<img src={require(`./assets/ig.svg`).default} alt="" />
+				</div>
+				<div className="links">
+					<ul>
+						<li>
+							<a href="">Privacy Policy</a>
+						</li>
+						<li>
+							<span className="hidden">|</span>
+							<a href="">Terms of Use</a>
+						</li>
+						<li>
+							<span className="hidden">|</span>
+							<a href="">CA Supply Chain Act</a>
+						</li>
+						<li>
+							<span className="hidden">|</span>
+							<a href="">Submit Your Idea</a>
+						</li>
+						<li>
+							<span className="hidden">|</span>
+							<a href="">Cookie Preferences</a>
+						</li>
+					</ul>
+					<p>© 2020 Starbucks Coffee Company. All rights reserved.</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Mainfooter;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import Menu from "./Menu";
 import Footer from "./Footer";
 import Textbanner from "./Textbanner";
 import Homebanner from "./Homebanner";
+import Mainfooter from "./Mainfooter";
 const App = () => {
 	return (
 		<div>
 			<Navigation />
 			<Textbanner />
 			<Homebanner />
+			<Mainfooter />
 			{/* <Footer /> */}
 		</div>
 	);
